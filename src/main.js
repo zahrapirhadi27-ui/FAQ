c23e441c55cd78cbfaeaf0dac138d99810157d36
zpirhadi <zahrapirhadi27@gmail.com>
2026-02-12 21:17:15 +0330

    v2

--- src/main.js
+++ src/main.js
@@ -1,15 +1,57 @@
 const lis1tEl = document.getElementById("list1");
 const p1El = document.getElementById("p1");
 const icon1El = document.getElementById("icon1");
-console.log(lis1tEl);
-console.log(p1El);
-console.log(icon1El);
+const list2El =document.getElementById("list2");
+const p2El = document.getElementById("p2");
+const icon2El = document.getElementById("icon2");
+const list3El = document.getElementById("list3");
+const p3El = document.getElementById("p3");
+const icon3El = document.getElementById("icon3");
+const list4El = document.getElementById("list4");
+const p4El = document.getElementById("p4");
+const icon4El = document.getElementById("icon4");
+const list5El = document.getElementById("list5");
+const p5El = document.getElementById("p5");
+const icon5El = document.getElementById("icon5");
+const list6El = document.getElementById("list6");
+const p6El = document.getElementById("p6");
+const icon6El = document.getElementById("icon6");
+const list7El = document.getElementById("list7");
+const p7El = document.getElementById("p7");
+const icon7El = document.getElementById("icon7");
 
 lis1tEl.addEventListener("click", () => {
   lis1tEl.classList.toggle("active");
   icon1El.classList.toggle("rotate-45")
 });
 
+list2El.addEventListener("click", () => {
+ list2El.classList.toggle("active");
+  icon2El.classList.toggle("rotate-45");
+});
 
+list3El.addEventListener("click", () => {
+  list3El.classList.toggle("active");
+  icon3El.classList.toggle("rotate-45");
+});
 
+list4El.addEventListener("click", () => {
+  list4El.classList.toggle("active");
+  icon4El.classList.toggle("rotate-45");
+});
+
+list5El.addEventListener("click", () => {
+  list5El.classList.toggle("active");
+  icon5El.classList.toggle("rotate-45");
+});
+
+list6El.addEventListener("click", () => {
+  list6El.classList.toggle("active");
+  icon6El.classList.toggle("rotate-45");
+});
+
+list7El.addEventListener("click", () => {
+  list7El.classList.toggle("active");
+  icon7El.classList.toggle("rotate-45");
+});
 
